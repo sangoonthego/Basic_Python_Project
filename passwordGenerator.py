72eb63d72ab310101b4fed09a5282421ad878c16
Nguyen Ngoc <nguyentuanngocc2005@gmail.com>
2025-06-18 17:26:05 +0700

    Push PasswordGeneator

---
+++ passwordGenerator.py
@@ -0,0 +1,41 @@
+import random
+import string
+
+def generate_password(min_length, numbers=True, special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
+
+    pwd = ""
+    has_number = False
+    has_special = False
+    meets_criteria = False
+
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
+
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_number
+        if special_characters:
+            meets_criteria = has_special
+
+    return pwd
+
+min_length = int(input("Enter the min length of password: "))
+has_number = input("Has number?: ").lower() == "y"
+has_special = input("Has special?: ").lower() == "y"
+pwd = generate_password(min_length, has_number, has_special)
+print(pwd)
